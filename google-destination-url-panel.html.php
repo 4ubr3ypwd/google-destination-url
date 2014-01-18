ef9fdbcd8b7ff99db6090bddf9b13ab8719cdea9
Aubrey Portwood <aubreypwd@gmail.com>
2014-01-18 16:39:33 -0700

    Converted translation strings that remained. Added deploy tool (to test).

--- google-destination-url-panel.html.php
+++ google-destination-url-panel.html.php
@@ -1,7 +1,7 @@
 <div id="search-panel">
 <p class="howto toggle-arrow toggle-arrow-active" 
 	id="internal-toggle">
-		Search 
+		<?php _e("Search",'gdurl'); ?> 
 		  <span style="color:blue">G</span><?php
 		?><span style="color:red">o<?php
 		?></span><span style="color:orange">o<?php
@@ -14,7 +14,9 @@
 <div id="search-panel-google-destination-url">
 	<div class="link-search-wrapper">
 		<label>
-			<span class="search-label">Search</span>
+			<span class="search-label">
+				<?php _e("Search",'gdurl'); ?>
+			</span>
 			<input type="search" 
 				id="search-field-google-destination-url" 
 				class="link-search-field" autocomplete="on">
@@ -27,7 +29,7 @@
 		<ul id="search-restults-google-destination-url-ul">
 			<li>
 				<span class="item-title">
-					Go search for something!
+					<?php _e("Go search for something!",'gdurl'); ?>
 				</span>
 			</li>
 		</ul>
