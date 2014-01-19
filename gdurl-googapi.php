170c39e0f15193a8c7a0ff5a9457d777c499993e
Aubrey Portwood <aubreypwd@gmail.com>
2014-01-19 12:28:02 -0700

    Moving functions around to their proper file.

--- gdurl-googapi.php
+++ gdurl-googapi.php
@@ -17,55 +17,9 @@ function gdurl_googapi($s){
 	curl_close($ch);
 
 	// Convert the response to JSON
-	$json = json_decode($body);	
+	$json = json_decode($body);
 
 	return apply_filters(__FUNCTION__,$json);
-}
-
-// Called from our Ajax part to load the results.
-function gdurl_googapi_load_results($s){	
-	$result_data = gdurl_googapi_cache($s);
-	include "gdurl-snippet.html.php";
-}
-
-// Differentiate whether we need to load from
-// cache or get live results.
-function gdurl_googapi_cache($s){
-	// First see if we have cached data (15 seconds?)
-	$diff_results = get_transient( 'gdurl_googapi_cache' );
-
-	if( isset($diff_results[$s]) ){
-		return apply_filters(
-			__FUNCTION__, 
-			$diff_results[$s]
-		);
-	}else{
-		return apply_filters(
-			'gdurl_cached_search_result_reset', 
-			gdurl_googapi_set_cache($s) 
-		);
-	}
-}
-
-// Sets the transient cache up by storing the results
-// of the search in a multi-array.
-function gdurl_googapi_set_cache($s){
-
-	// Get current transient data
-	$googapi_cache = get_transient( 'gdurl_googapi_cache' );
-
-	// Add this search
-	$googapi_cache[$s] = gdurl_googapi($s);
-
-	// Set the cache
-	set_transient( 
-		'gdurl_googapi_cache', 
-		$googapi_cache, 
-		10
-	);
-
-	// Return the results
-	return apply_filters(__FUNCTION__, $googapi_cache[$s]);
 }
 
 ?>
--- gdurl.php
+++ gdurl.php
@@ -1,5 +1,6 @@
 <?php 
 
+// Setup the JS
 function gdurl_js($hook){
 
 	// These are the admin pages we want 
@@ -65,5 +66,50 @@ add_action(
 	'gdest_url_googapi'
 );
 
+// Called from our Ajax part to load the results.
+function gdurl_googapi_load_results($s){	
+	$result_data = gdurl_googapi_cache($s);
+	include "gdurl-snippet.html.php";
+}
+
+// Differentiate whether we need to load from
+// cache or get live results.
+function gdurl_googapi_cache($s){
+	// First see if we have cached data (15 seconds?)
+	$diff_results = get_transient( 'gdurl_googapi_cache' );
+
+	if( isset($diff_results[$s]) ){
+		return apply_filters(
+			__FUNCTION__, 
+			$diff_results[$s]
+		);
+	}else{
+		return apply_filters(
+			'gdurl_cached_search_result_reset', 
+			gdurl_googapi_set_cache($s) 
+		);
+	}
+}
+
+// Sets the transient cache up by storing the results
+// of the search in a multi-array.
+function gdurl_googapi_set_cache($s){
+
+	// Get current transient data
+	$googapi_cache = get_transient( 'gdurl_googapi_cache' );
+
+	// Add this search
+	$googapi_cache[$s] = gdurl_googapi($s);
+
+	// Set the cache
+	set_transient( 
+		'gdurl_googapi_cache', 
+		$googapi_cache, 
+		10
+	);
+
+	// Return the results
+	return apply_filters(__FUNCTION__, $googapi_cache[$s]);
+}
 
 ?>
